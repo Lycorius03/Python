e11006f90eb8948ffafd91c3175b631ef0fa18cb
Lycorius03 <Lycorius03@outlook.com>
2026-01-09 21:40:57 +0800

    change bank.py in the bank and add test_bank

--- bank/bank.py
+++ bank/bank.py
@@ -1,9 +1,10 @@
 def main():
-  greetings = input("Greeting:").strip().lower()
+  greetings = input("Greeting:")
   money = determine(greetings)
   print(money)
 
 def determine(Greetings):
+  Greetings = Greetings.strip().lower()
   if Greetings.startswith('hello'):
     return '0$'
   elif Greetings.startswith('h'):
